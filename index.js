fb3298326fce4f808ad3b952caa48e0f15a5deda
shakibnymr <shakibnymr11@gmail.com>
2023-08-30 16:41:38 +0600

    practice time

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mainNews.forEach(newsCard => {
 const card = document.createElement('div');
 card.classList = `card w-96 bg-base-100 shadow-xl`;
 card.innerHTML = `
-<figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+<figure><img src="${newsCard.image_url}" alt="Shoes" /></figure>
 <div class="card-body">
   <h2 class="card-title">Shoes!</h2>
   <p>If a dog chews shoes whose shoes does he choose?</p>
